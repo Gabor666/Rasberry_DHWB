c9d8435d2fc49913c622ebb0d81d7728b68a01ad
Gabor666 <gubigabi00@gmail.com>
2025-03-03 10:24:32 +0100

    Version 2

--- tempV2.sh
+++ tempV2.sh
@@ -3,7 +3,8 @@
 while true;  do
 vartemp=`cat /sys/bus/w1/devices/28-00000a54a3a5/w1_slave | grep t | sed 's/.*t=\([0-9][0-9]\)\([0-9][0-9][0-9]\)/\1.\2/'`
 rrdupdate /home/gabor/temperatur1.rrd N:$vartemp
-sleep 5
+sleep  7
+
 echo $vartemp 
 
 done 
